1dda9a29a329a71720f66c0322d95716b39a7402
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2021-02-19 22:10:28 +0800

    --

--- profile
+++ profile
@@ -2,6 +2,7 @@
 #npm环境变量
 NPM_PACKAGES="${HOME}/.config/npm-packages"
 PATH="$NPM_PACKAGES/bin:$PATH"
-
 unset MANPATH
 export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+
+#export WINEARCH=win32
--- tag-zsh/config/zsh/alias.zsh
+++ tag-zsh/config/zsh/alias.zsh
@@ -7,13 +7,13 @@ alias -s rar='rar x'
 alias -s 7z='7z x'
 
 #ssh
-alias vps='ssh root@204.44.83.246'
-alias pi='ssh pi@192.168.0.2'
+alias vps='ssh root@pr.re-blog.tk'
+alias pi='ssh pi@192.168.3.2'
 
 #alias
 alias la='ls -a'
 alias ll='ls -al'
-alias wine5='WINEPREFIX=~/.wine5 deepin-wine5'
+alias wine5='WINEPREFIX=~/.Wines/.wine5 deepin-wine5'
 alias ra=ranger
 alias s=neofetch
 alias scfg=/home/rewrite/scripts/switchcfg.sh
@@ -27,8 +27,8 @@ alias ic=/home/rewrite/scripts/img-compression.sh
 alias cl=/home/rewrite/scripts/clash.sh
 alias kl=/home/rewrite/scripts/keyboard-light.sh
 #翻译
-alias en='trans en:zh'
-alias zh='trans zh:en'
+alias te='trans en:zh'
+alias tz='trans zh:en'
 #live_wallpaper控制台
 alias vwc=/home/rewrite/prog/dwm/scripts/live-wallpaper-console.sh
 #crontab
