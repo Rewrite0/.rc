b26be6a4ab90fff530fd49c2207e8782e6f946fe
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2022-06-26 17:19:47 +0800

    --

--- tag-zsh/config/zsh/aliases.zsh
+++ tag-zsh/config/zsh/aliases.zsh
@@ -11,35 +11,31 @@ alias la='ls -a'
 alias ll='ls -al'
 alias ra=ranger
 alias s=neofetch
-alias scfg=/home/rewrite/scripts/switchcfg.sh
-alias mu=/home/rewrite/scripts/mount-disk.sh
+alias scfg=/home/re/scripts/switchcfg.sh
+alias mu=/home/re/scripts/mount-disk.sh
 alias lg=lazygit
-alias il=/home/rewrite/scripts/imglink.sh
+alias il=/home/re/scripts/imglink.sh
 alias ngi='nvim .gitignore'
-alias rm=/home/rewrite/scripts/trash.sh
-alias rrm=/usr/bin/rm
+alias update='sudo reflector --verbose --country China -l 10 --sort rate --save /etc/pacman.d/mirrorlist'
 # 显卡切换
-alias sn='/home/rewrite/scripts/switchGPU.sh nvidia'
-alias si='/home/rewrite/scripts/switchGPU.sh integrated'
-# wine
-alias wine5='WINEPREFIX=~/.Wines/wine5 deepin-wine5'
-alias wine64='WINEARCH=win64 WINEPREFIX=~/.Wines/wine64 wine'
+alias sn='/home/re/scripts/switchGPU.sh nvidia'
+alias si='/home/re/scripts/switchGPU.sh integrated'
 # 图片压缩
-alias ic=/home/rewrite/scripts/img-compression.sh
+alias ic=/home/re/scripts/img-compression.sh
 # clash
 alias cls='systemctl status clash'
 alias clr='systemctl restart clash'
 # 外接键盘光
-alias kl=/home/rewrite/scripts/keyboard-light.sh
+alias kl=/home/re/scripts/keyboard-light.sh
 #翻译
 alias te='trans en:zh'
 alias tz='trans zh:en'
 #live_wallpaper控制台
-alias vwc=/home/rewrite/prog/dwm/scripts/live-wallpaper-console.sh
+alias vwc=/home/re/prog/dwm/scripts/live-wallpaper-console.sh
 #crontab
-alias cronl='sudo crontab -u rewrite -l'
-alias crone='sudo crontab -u rewrite -e'
-alias cron='sudo crontab -u rewrite'
+alias cronl='sudo crontab -u re -l'
+alias crone='sudo crontab -u re -e'
+alias cron='sudo crontab -u re'
 # tmux
 alias tn='tmux new -s'
 alias ta='tmux attach -t'
